afe577c984466748cb64ce8aa7e8a18dfc9a1cd7
kentaurus44 <rigil44@gmail.com>
2014-12-27 21:00:19 -0500

    Basic Player Movement Done

---
+++ Platformer/Assets/Scripts/Core/Terrain/Terrain.cs
@@ -0,0 +1,37 @@
+﻿// File: Terrain
+// Created by: Rigil Malubay
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Terrain : MonoBehaviour {
+	
+	// const
+
+	// enum
+	public enum eTerrainType
+	{
+		FLOOR,
+		WALL
+	}
+
+	// public
+	public eTerrainType m_Type = eTerrainType.FLOOR;
+
+	// protected
+	
+	// private
+
+	#region Unity API
+	#endregion
+
+	#region Public Methods
+	#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
