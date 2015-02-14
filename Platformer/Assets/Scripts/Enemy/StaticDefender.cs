7249fbfa1beae6a4b33f4a66bf5dca1439d3c941
kentaurus44 <rigil44@gmail.com>
2015-02-13 20:38:45 -0500

    Goes with previous commit

--- Platformer/Assets/Scripts/Enemy/StaticDefender.cs
+++ Platformer/Assets/Scripts/Enemy/StaticDefender.cs
@@ -25,6 +25,7 @@ public class StaticDefender : Enemy {
 	private bool m_Loaded = false;
 	private bool m_TargetLocated = false;
 	private Vector3 m_TargetPosition;
+	private Collider[] m_Colliders;
 
 	#region Unity API
 	protected override void Start()
@@ -43,6 +44,7 @@ public class StaticDefender : Enemy {
 			m_RechargeTimer.Update();
 		}
 
+		FindTargets();
 		AquireTarget();
 
 		if (m_Loaded && m_TargetLocated)
@@ -64,25 +66,29 @@ public class StaticDefender : Enemy {
 	{
 		m_Loaded = true;
 	}
+	private void FindTargets()
+	{
+		m_Colliders = Physics.OverlapSphere(transform.position, m_RangeOfVision);
+	}
 	private void AquireTarget()
 	{
-		for (int i = 0 ; i < 360 ; ++i)
+		foreach (Collider c in m_Colliders)
 		{
-			Ray ray = new Ray(transform.position, new Vector3(Mathf.Cos(i), Mathf.Sin(i)));
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit, m_RangeOfVision, m_Targets))
+			GameObject obj = c.gameObject;
+			if (obj.GetComponent<PlayerController>() != null)
 			{
-				Debug.DrawLine(ray.origin, hit.point);
-
-				m_TargetLocated = true;
-
-				m_TargetPosition = hit.transform.position;
-
-				return;
+				m_TargetPosition = obj.transform.position;
+				Ray ray = new Ray(transform.position, m_TargetPosition);
+				if (Physics.Raycast(ray, m_Targets))
+				{
+					m_TargetLocated = true;
+					return;
+				}
 			}
 		}
 		m_TargetLocated = false;
 	}
+
 	private void Shoot()
 	{
 		if (m_Projectile != null)
@@ -91,7 +97,6 @@ public class StaticDefender : Enemy {
 			obj.transform.position = this.transform.position;
 			obj.transform.parent = this.transform.parent;
 			Projectile projectile = obj.GetComponent<Projectile>();
-//			m_Projectile.RegisterObserver(this);
 			projectile.TargetPosition = m_TargetPosition;
 			projectile.Shoot();
 		}
@@ -99,4 +104,37 @@ public class StaticDefender : Enemy {
 		m_RechargeTimer.Start();
 	}
 	#endregion
+
+
+//	// Get all the objects in range
+//	Collider[] cols = Physics.OverlapSphere(transform.position, range);
+//	// Assign the first object as the nearest for now
+//	Collider nearest = cols[0];
+//	
+//	// Loop through objects
+//	foreach (Collider c in cols)
+//	{
+//		// Get the current objects rigidbody
+//		Rigidbody rb = c.attachedRigidbody;
+//		// Get the force between the objects
+//		Vector3 offset = transform.position - c.transform.position;
+//		// Get the square of the force (distance)
+//		float sqmag = offset.sqrMagnitude;
+//		
+//		// if this object is closer
+//		if (rigidbody != rb && sqmag < nearest.transform.position.sqrMagnitude && rb != rigidbody && rb.tag != "Player")
+//		{
+//			nearest = c; //assign this as the new nearest
+//		}
+//	}
+//	// double check that we are not refering to self
+//	if (nearest.rigidbody != rigidbody)
+//	{
+//		//get the force of the nearest object
+//		Vector3 force = transform.position - nearest.transform.position;
+//		// Normalize the force
+//		force.Normalize();
+//		// Apply the force
+//		rigidbody.AddForce(GravityConstant * force * rigidbody.mass * nearest.rigidbody.mass / force.sqrMagnitude);
+//	}
 }
