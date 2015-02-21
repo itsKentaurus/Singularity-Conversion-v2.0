2f3574106f0790b30b02eee94d80613a37d1560a
kentaurus44 <rigil44@gmail.com>
2015-02-21 16:12:12 -0500

    Woot Optimization!! + In CameraDetection.cs will only make the object show when it's in range of the camera.

---
+++ Platformer/Assets/Module/Platformer/Scripts/Camera/CameraDetection.cs
@@ -0,0 +1,48 @@
+﻿// File: CameraDetection
+// Created by: Rigil Malubay
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraDetection : MonoBehaviour {
+	
+	// const
+
+	// enum
+
+	// public
+
+	// protected
+
+	// private
+
+	#region Unity API
+	protected void OnTriggerEnter(Collider collider)
+	{
+		MeshRenderer mesh = collider.gameObject.GetComponent<MeshRenderer>();
+		if (mesh != null)
+		{
+			mesh.enabled = true;
+		}
+	}
+
+	protected void OnTriggerExit(Collider collider)
+	{
+		MeshRenderer mesh = collider.gameObject.GetComponent<MeshRenderer>();
+		if (mesh != null)
+		{
+			mesh.enabled = false;
+		}
+	}
+	#endregion
+
+	#region Public Methods
+	#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
