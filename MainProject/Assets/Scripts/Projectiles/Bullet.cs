720728de03e7e3f9e5640089e72a98b181d576b7
Rigil <rigil44@gmail.com>
2016-09-11 16:25:40 -0400

    Shooting logic
    Little shooting logic
    Nothing tested

---
+++ MainProject/Assets/Scripts/Projectiles/Bullet.cs
@@ -0,0 +1,27 @@
+﻿//
+// Script name: Bullet
+//
+//
+// Programmer: Kentaurus
+//
+
+using UnityEngine;
+using System.Collections;
+
+public class Bullet : Shooter.BasicProjectile
+{
+	#region Variables
+	#endregion
+
+	#region Unity API
+	#endregion
+
+	#region Public Methods
+	#endregion
+
+	#region Protected Methods
+	#endregion
+
+	#region Private Methods
+	#endregion
+}
